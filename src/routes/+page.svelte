0e8862355a4084f538e6c0aa4d6ce481ee60d8bb
colecrouter <colecrouter1@gmail.com>
2023-12-19 10:02:05 -0600

    Refactor list-group in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,9 @@
     <h1>Advent of Svelte 2023</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-    <ol class="list-group-flush list-group-numbered">
-            {#each data.paths as path}
-                <li class="list-group-item">
-                    <a href={path.replace('+page.svelte', '')}>Day {(/\.\/(\d+)\/\+page.svelte/.exec(path))?.[1]}</a>
-                </li>
-            {/each}
+    <ol class="list-group list-group-flush list-group-numbered">
+        {#each data.paths as path}
+            <a class="list-group-item list-group-item-action" href={path.replace('+page.svelte', '')}>Day {(/\.\/(\d+)\/\+page.svelte/.exec(path))?.[1]}</a>
+        {/each}
     </ol>
 </div>
